c70d10184881290aed7fb59f5a2952a53358c26f
Jween44 <jmerida9@ivytech.edu>
2025-07-24 21:29:23 -0400

    Add files via upload

---
+++ Module 7 Assignment - Python using loops - Joshua Merida.py
@@ -0,0 +1,10 @@
+numbersInList = [ 4, 44,5,2,1,3,4,8,9,2,5,11,120,45,84]
+for list in numbersInList:
+    if list % 2 == 0:
+        print ( list ,"  is even ")
+    else:
+        print ( list ,"is odd")
+
+             
+
+    
